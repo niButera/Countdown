11f9a90c99b5ae23180df476153bdd65c5bda62b
NiButera <nikolasxp683@gmail.com>
2023-03-19 01:55:08 -0300

    Arrumei a contagem regressiva para esperar o pressionamento de uma tecla

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
 ﻿#pragma warning disable
-Console.Beep(1320, 100); Console.Write("Preparar");
+Console.Beep(1320, 100); Console.WriteLine("Preparar...");
+Console.ReadKey(true);
+Console.Beep(1650, 200); Console.WriteLine("Apontar...");
+Console.ReadKey(true);
+Console.Beep(2200, 600); Console.WriteLine("FOGO!");
 
-Thread.Sleep(1000); Console.Write(".");
-Thread.Sleep(1000); Console.Write(".");
-Thread.Sleep(1000); Console.WriteLine(".");
-
-Console.Beep(1650, 200); Console.Write("Apontar");
-
-Thread.Sleep(1000); Console.Write(".");
-Thread.Sleep(1000); Console.Write(".");
-Thread.Sleep(1000); Console.WriteLine(".");
-
-Console.Beep(2200, 600); Console.Write("FOGO!");
-
-//Manas entendi tudor!!!
