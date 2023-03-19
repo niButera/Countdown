dd308bdd851f82f7ecbe4b7e6d77418aec3b84e8
NiButera <nikolasxp683@gmail.com>
2023-03-19 01:00:11 -0300

    Fiz a contagem regressiva com som

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable
+Console.Beep(1320, 100); Console.Write("Preparar");
+
+Thread.Sleep(1000); Console.Write(".");
+Thread.Sleep(1000); Console.Write(".");
+Thread.Sleep(1000); Console.WriteLine(".");
+
+Console.Beep(1650, 200); Console.Write("Apontar");
+
+Thread.Sleep(1000); Console.Write(".");
+Thread.Sleep(1000); Console.Write(".");
+Thread.Sleep(1000); Console.WriteLine(".");
+
+Console.Beep(2200, 600); Console.Write("FOGO!");
+
+//Manas entendi tudor!!!
